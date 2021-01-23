c9bd53265e9289502e1c5ab64d415ae232d4a45e
Martin Fenner <martin.fenner@datacite.org>
2021-01-23 14:58:54 +0100

    Update IndexNavbar.js

--- components/Navbars/IndexNavbar.js
+++ components/Navbars/IndexNavbar.js
@@ -63,16 +63,6 @@ export default function Navbar(props) {
                   <span className="lg:hidden inline-block ml-2">Feed</span>
                 </a>
               </li>
-
-              <li className="flex items-center">
-                <a
-                  className="inline-block text-white bg-gray-800 active:bg-gray-700 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
-                  href="https://www.creative-tim.com/product/notus-nextjs?ref=nnjs-index"
-                  target="_blank"
-                >
-                  <i class="fas fa-arrow-alt-circle-down"></i> Download
-                </a>
-              </li>
             </ul>
           </div>
         </div>
