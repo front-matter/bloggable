f32d21167d582cfcd318b9b0cc82f3dde660e5d2
Martin Fenner <martin.fenner@datacite.org>
2021-01-30 17:39:41 +0100

    updated root url

--- components/Navbars/IndexNavbar.js
+++ components/Navbars/IndexNavbar.js
@@ -46,7 +46,7 @@ export default function Navbar(props) {
               <li className="flex items-center">
                 <a
                   className="hover:text-gray-600 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold border-b-0"
-                  href="https://feedly.com/i/subscription/feed/https://blog.martinfenner.org/rss/"
+                  href="https://feedly.com/i/subscription/feed/https://martinfenner.ghost.io/rss/"
                   target="_blank"
                 >
                   <i className="text-gray-500 fas fa-rss text-lg leading-lg " />
