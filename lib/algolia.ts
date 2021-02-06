cb20a0939da91244f73069ef994e00335973f823
Martin Fenner <martin.fenner@datacite.org>
2021-02-06 22:07:09 +0100

    show description as text

--- lib/algolia.ts
+++ lib/algolia.ts
@@ -1,10 +1,13 @@
 import { getPosts } from './posts'
-import truncate from 'lodash/truncate'
+import trimText from './trimText'
+import sanitizeHtml from 'sanitize-html'
 const fs = require('fs')
 
 export async function updateIndex() {
   const posts = await getPosts()
   const objects = posts.reduce((accumulator, currentValue) => {
+    const description = trimText(sanitizeHtml(currentValue.html, { allowedTags: [ 'b', 'i', 'em', 'strong']}), 200, 250, 300)[0]
+
     accumulator.push({
       objectID: currentValue.uuid,
       title: currentValue.title,
@@ -14,10 +17,7 @@ export async function updateIndex() {
         name: currentValue.primary_author.name,
         imageUrl: 'https:' + currentValue.primary_author.profile_image
       },
-      description: truncate(currentValue.html, {
-        length: 500,
-        omission: '***'
-      }),
+      description: description + '...',
       content: currentValue.html,
       readingTime: currentValue.reading_time,
       _tags: currentValue.tags.map((tag) => tag.name),
