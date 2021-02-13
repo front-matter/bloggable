15b7a0324e25b24d3f1dc22fe8bbbd27b70ba1ca
Martin Fenner <martin.fenner@datacite.org>
2021-02-13 20:18:32 +0100

    update index

--- lib/pandoc.ts
+++ lib/pandoc.ts
@@ -1,34 +1,79 @@
 import { getPosts } from './posts'
-import nodePandoc from 'node-pandoc'
+import nodePandoc from 'node-pandoc-promise'
 
 export async function generateEpub() {
   const posts = await getPosts()
-  let src, date, epubArgs, pdfArgs, jatsArgs, callback
+  let src, date, epubArgs, pdfArgs, jatsArgs
 
-  callback = (err, result) => {
-    if (err) console.error(err)
-    return result
-  }
-
-  posts.forEach((post) => {
+  await posts.forEach((post) => {
     src = post.html
 
     date = new Date(post.published_at).toLocaleDateString('en-US', {
-                    year: 'numeric',
-                    month: 'long',
-                    day: 'numeric'
-                  })
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    })
 
     // generate ePub
-    epubArgs = ['-f', 'html', '-t', 'epub', '-o', './public/epub/' + post.slug + '.epub', '--standalone', '--data-dir', './']
-    nodePandoc(src, epubArgs, callback)
+    epubArgs = [
+      '-f',
+      'html',
+      '-t',
+      'epub',
+      '-o',
+      './public/epub/' + post.slug + '.epub',
+      '--standalone',
+      '--data-dir',
+      './'
+    ]
+    nodePandoc(src, epubArgs)
 
     // generate pdf
-    pdfArgs = ['-f', 'html', '-t', 'pdf', '-o', './public/pdf/' + post.slug + '.pdf', '--standalone', '--data-dir', './', '--metadata', 'title=' + post.title, '--metadata', 'author=' + post.primary_author.name + ', Gobbledygook', '--metadata', 'date=' + date]
-    nodePandoc(src, pdfArgs, callback)
+    pdfArgs = [
+      '-f',
+      'html',
+      '-t',
+      'pdf',
+      '-o',
+      './public/pdf/' + post.slug + '.pdf',
+      '--standalone',
+      '--data-dir',
+      './',
+      '--metadata',
+      'title=' + post.title,
+      '--metadata',
+      'author=' + post.primary_author.name + ', Gobbledygook',
+      '--metadata',
+      'date=' + date
+    ]
+    nodePandoc(src, pdfArgs)
 
     // generate jats
-    const jatsArgs = ['-f', 'html', '-t', 'jats', '-o', './public/jats/' + post.slug + '.xml', '--standalone', '--data-dir', './', '--metadata', 'title=' + post.title, '--metadata', 'author=' + post.primary_author.name, '--metadata', 'date=' + date, '--metadata', 'journal.title=Gobbledygook', '--metadata', 'license.type=Open Access', '--metadata', 'license.link=https://creativecommons.org/licenses/by/4.0/legalcode', '--metadata', 'license.text=Distributed under the terms of the Creative Commons Attribution 4.0 License.',]
-    nodePandoc(src, jatsArgs, callback)
+    jatsArgs = [
+      '-f',
+      'html',
+      '-t',
+      'jats',
+      '-o',
+      './public/jats/' + post.slug + '.xml',
+      '--standalone',
+      '--data-dir',
+      './',
+      '--metadata',
+      'title=' + post.title,
+      '--metadata',
+      'author=' + post.primary_author.name,
+      '--metadata',
+      'date=' + date,
+      '--metadata',
+      'journal.title=Gobbledygook',
+      '--metadata',
+      'license.type=Open Access',
+      '--metadata',
+      'license.link=https://creativecommons.org/licenses/by/4.0/legalcode',
+      '--metadata',
+      'license.text=Distributed under the terms of the Creative Commons Attribution 4.0 License.'
+    ]
+    nodePandoc(src, jatsArgs)
   })
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,18 @@ import React from 'react'
 import Link from 'next/link'
 import ReactHtmlParser from 'react-html-parser'
 import algoliasearch from 'algoliasearch/lite'
-import { InstantSearch, connectHits, connectStats, connectPagination } from 'react-instantsearch-dom'
+import {
+  InstantSearch,
+  connectHits,
+  connectStats,
+  connectPagination
+} from 'react-instantsearch-dom'
 
 import { getPosts } from '../lib/posts'
 import { pluralize } from '../lib/helpers'
 import { generateRssFeed } from '../lib/feed'
 // import { generateEpub } from '../lib/pandoc'
-// import { updateIndex } from '../lib/algolia'
+import { updateIndex } from '../lib/algolia'
 import IndexNavbar from '../components/Navbars/IndexNavbar.js'
 import Footer from '../components/Footers/Footer.js'
 import Byline from '../components/Byline'
@@ -24,7 +29,7 @@ export async function getStaticProps(context) {
 
   await generateRssFeed()
   // await generateEpub()
-  // await updateIndex()
+  await updateIndex()
 
   return {
     props: { posts }
@@ -43,7 +48,7 @@ const IndexPage = (props) => {
         <div key={hit.uuid}>
           <h1>
             <Link href={`/posts/${hit.slug}`}>
-              <a className="text-2xl border-b-0 font-sans font-bold no-underline sm:text-4xl hover:underline">
+              <a className="text-2xl leading-tight border-b-0 font-sans font-bold no-underline sm:text-4xl hover:underline">
                 {hit.title}
               </a>
             </Link>
@@ -84,30 +89,36 @@ const IndexPage = (props) => {
 
   const CustomStats = connectStats(Stats)
 
-  const Pagination = ({ currentRefinement, nbPages, refine, createURL, showFirst }) => {
+  const Pagination = ({
+    currentRefinement,
+    nbPages,
+    refine,
+    createURL,
+    showFirst
+  }) => {
     if (nbPages == 1) return null
 
     return (
       <div className="py-2 mt-4">
         <nav className="block">
           <ul className="flex pl-0 rounded list-none flex-wrap">
-          {new Array(nbPages).fill(null).map((_, index) => {
-            const page = index + 1
+            {new Array(nbPages).fill(null).map((_, index) => {
+              const page = index + 1
 
-            return (
-              <li key={index}>
-                <a
-                  href={createURL(page)}
-                  className="first:ml-0 text-base font-semibold flex w-8 h-8 mx-4 p-0 rounded-full items-center justify-center relative border-2 border-solid border-blue-600 text-blue-600"
-                  onClick={event => {
-                    event.preventDefault();
-                    refine(page);
-                  }}
-                >
-                  {page}
-                </a>
-              </li>
-            )
+              return (
+                <li key={index}>
+                  <a
+                    href={createURL(page)}
+                    className="first:ml-0 text-base font-semibold flex w-8 h-8 mx-4 p-0 rounded-full items-center justify-center relative border-2 border-solid border-blue-600 text-blue-600"
+                    onClick={(event) => {
+                      event.preventDefault()
+                      refine(page)
+                    }}
+                  >
+                    {page}
+                  </a>
+                </li>
+              )
             })}
           </ul>
         </nav>
