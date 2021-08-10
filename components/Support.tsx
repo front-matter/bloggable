003de4255f8a8f2be7b8ee7ec94af073c8e88711
Martin Fenner <martin@front-matter.io>
2021-08-10 05:44:27 +0200

    Update Support.tsx

--- components/Support.tsx
+++ components/Support.tsx
@@ -9,23 +9,27 @@ export default function Support() {
             <h1 className="text-5xl font-extrabold text-green-600 sm:text-center">
               Support Front Matter
             </h1>
-            <p className="mt-5 text-xl text-gray-500 sm:text-center">
-              Support the Front Matter Blog by a one-time donation or regular
-              membership via{' '}
-              <img
-                className="inline"
-                src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
-                alt="Buy me a coffee"
-              />{' '}
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href="https://www.buymeacoffee.com/frontmatter"
-              >
-                Buy me a coffee
-              </a>
-              .
-            </p>
+            <div className="mt-5 text-xl text-gray-500 sm:text-center">
+              <p>
+                Support the Front Matter Blog by a one-time donation or regular
+                membership via
+              </p>
+              <p>
+                <img
+                  className="inline"
+                  src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
+                  alt="Buy Me a Coffee"
+                />{' '}
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://www.buymeacoffee.com/frontmatter"
+                >
+                  Buy Me a Coffee
+                </a>
+                .
+              </p>
+            </div>
           </div>
         </div>
       </div>
