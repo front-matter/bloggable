1ce2633139763db00c6e73ea885ab629ba1a0a59
Martin Fenner <martin@front-matter.io>
2021-12-09 13:13:00 +0100

    Update Newsletter.tsx

--- components/Newsletter.tsx
+++ components/Newsletter.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Link from 'next/link'
+// import Link from 'next/link'
 
 export default function Newsletter() {
   const [message, setMessage] = useState(null)
