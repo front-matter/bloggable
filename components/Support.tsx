19711e60f6bbc95fe1f010c8791660eac6d05c05
Martin Fenner <martin@front-matter.io>
2021-12-09 13:29:27 +0100

    align text

--- components/Support.tsx
+++ components/Support.tsx
@@ -6,28 +6,15 @@ export default function Support() {
       <div className="bg-white">
         <div className="container max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8 font-sans">
           <div className="sm:flex sm:flex-col sm:align-center">
-            <h1 className="text-5xl font-extrabold text-green-600 sm:text-center">
+            <h1 className="text-5xl font-extrabold text-green-600">
               Support Front Matter
             </h1>
-            <div className="mt-5 text-xl text-gray-500 sm:text-center">
+            <div className="mt-5 text-xl text-gray-500">
               <p>
-                Support the Front Matter Blog by a one-time donation or regular
-                membership via
-              </p>
-              <p>
-                <img
-                  className="inline"
-                  src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
-                  alt="Buy Me a Coffee"
-                />{' '}
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://www.buymeacoffee.com/frontmatter"
-                >
-                  Buy Me a Coffee
-                </a>
-                .
+                Support of the Front Matter Blog by a one-time donation or
+                regular membership via <strong>Buy me a Coffee</strong> has been
+                discontinued on December 9, 2021. Instead, you can sign up for
+                free Front Matter membership via a signup form on the homepage.
               </p>
             </div>
           </div>
