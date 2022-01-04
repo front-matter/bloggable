146d9820ff938e666d3587ea7b210ead6deee1b6
Martin Fenner <martin@front-matter.io>
2022-01-04 12:38:25 +0000

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -43,9 +43,15 @@ const Header = ({ tags, tag }) => {
                         height={32}
                         width={32}
                       />
+                      {tag && tag.name ? (
+                        <span className="text-lg font-semibold font-sans pl-1 pt-1 hidden md:inline">
+                          Front Matter Blog
+                        </span>
+                      ) : (
                         <span className="text-lg font-semibold font-sans pl-1 pt-1">
                           Front Matter Blog
                         </span>
+                      )}
                     </a>
                   </Link>
                 </div>
