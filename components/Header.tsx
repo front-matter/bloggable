a77f7f982891dfca9b58d96d17461c03c85a1301
Martin Fenner <martin@front-matter.io>
2022-07-26 16:03:43 +0200

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,6 +13,7 @@ function classNames(...classes) {
 
 const Header = ({ tags, tag }) => {
   const [query, setQuery] = useQueryState('query')
+  console.log(query)
   const inputRef = useRef<HTMLInputElement>(null)
 
   const onSubmit = () => {
