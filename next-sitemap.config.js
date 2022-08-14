cf612c3dc6b58aeaac1d78e4ecf1bfe046acd7c3
Martin Fenner <martin@front-matter.io>
2022-08-14 09:41:52 +0200

    upgrade packages

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: process.env.SITE_URL || 'https://blog.front-matter.io',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
+
+export default config
