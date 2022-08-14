d0e264da74512fcc134d19bc9b16a8f55218ec0c
Martin Fenner <martin@front-matter.io>
2022-08-14 11:29:07 +0200

    fix sitemap configuration

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
   siteUrl: process.env.SITE_URL || 'https://blog.front-matter.io',
-  generateRobotsTxt: true, // (optional)
-  // ...other options
+  generateRobotsTxt: true,
+  exclude: [
+    '/features',
+    '/pricing',
+    '/resources',
+    '/open-source',
+    '/support',
+    '/team',
+    '/categories/*'
+  ],
 }
 
 export default config
